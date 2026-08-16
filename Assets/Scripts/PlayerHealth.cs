12f9ea3ec92684382a1614bab56b695f8c3d576b
Ayush_K <ayush.kuppanda@student.saedubai.com>
2026-08-16 05:22:22 +0400

    player health bar

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
+    public Slider healthBar; // drag your HealthBar Slider here
 
     void Start()
     {
         currentHealth = maxHealth;
+
+        if (healthBar != null)
+        {
+            healthBar.maxValue = maxHealth;
+            healthBar.value = currentHealth;
+        }
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        if (healthBar != null)
+        {
+            healthBar.value = currentHealth;
+        }
+
         Debug.Log("Player took damage: " + amount + " | HP: " + currentHealth);
 
         if (currentHealth <= 0)
@@ -24,6 +39,5 @@ public class PlayerHealth : MonoBehaviour
     void Die()
     {
         Debug.Log("Player died");
-        // hook in death animation / game over logic later
     }
 }
